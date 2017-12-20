7f72eeafc8e1d5bcbef3562768565aad664b6696
Guilherme Recchi Cardozo <gcardozo@inf.ufsm.br>
2017-12-19 23:36:38 -0200

    Adding the project.

---
+++ Run.bat
@@ -0,0 +1,4 @@
+@echo off
+cd bin
+C:\HaxeToolkit\hl_1.3\hl heaps-hashlink-template.hl
+pause
---
+++ src/Main.hx
@@ -0,0 +1,25 @@
+ class Main extends hxd.App 
+ {
+	var bmp : h2d.Bitmap;
+	override function init() 
+	{
+		// allocate a Texture with red color and creates a 100x100 Tile from it
+		var tile = h2d.Tile.fromColor(0xFF0000, 100, 100);
+		// create a Bitmap sprite, which will display the tile
+		// and will be added to our 2D scene (s2d)
+		bmp = new h2d.Bitmap(tile, s2d);
+		// modify the display position of the Bitmap sprite
+		bmp.x = s2d.width * 0.5;
+		bmp.y = s2d.height * 0.5;
+	}
+	// on each frame
+	override function update(dt:Float) 
+	{
+		// increment the display bitmap rotation by 0.1 radians
+		bmp.rotation += 0.1;
+	}
+	static function main() 
+	{
+		new Main();
+	}
+}
